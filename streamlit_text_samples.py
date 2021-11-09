6f1179d3f336cfc19f7a4a860d34b8cc2e767d4d
shreyas0906 <shreyas0906@gmail.com>
2021-11-09 15:34:40 +0530

    ignore memory profiling

--- streamlit_text_samples.py
+++ streamlit_text_samples.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 # import plotly.express as px
-from memory_profiler import profile
+# from memory_profiler import profile
 
 
 #@profile
